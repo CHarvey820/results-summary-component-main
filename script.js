50452a61cbc388c33fdaaff0dd8283f02d7b838e
CHarvey820 <charvey820@gmail.com>
2024-04-24 15:29:07 -0500

    implement javascript, update readme

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
-document.addEventListener("DOMContentLoaded",getData);
-let jsonData = [];
+document.addEventListener("DOMContentLoaded", getData);
+
+const scoreTotal = document.querySelector(".scoreTotal");
+
+let average = 0;
 
 async function getData() {
-    const response = await fetch("./data.json");
-    const results = await response.json();
-    parseResults(results);
-  }
-
-
-function parseResults(results){
-    jsonData = results;
-    const testData = jsonData[0].category;
-    console.log(testData);
+  const response = await fetch("./data.json");
+  const results = await response.json();
+  parseResults(results);
 }
 
+function parseResults(results) {
+  getAverage(results);
 
+  results.forEach((result) => {
+    let strToUpdate = (result.category.toLowerCase());
+    document.querySelector(`.${strToUpdate}Score`).innerHTML += `
+    ${result.score} <span class="scoreGray"> / 100</span>
+    `;
+
+    document.querySelector(`.${strToUpdate}IconTitle`).innerHTML += `
+    <img src=${result.icon} alt="icon for ${result.category}" />${result.category}
+    `;
+  });
+}
+
+function getAverage(results) {
+  let accum = 0;
+
+  results.forEach((result) => {
+    accum += Number(result.score);
+  });
+
+  average = Math.round(accum / 4);
+  scoreTotal.innerHTML += `
+  <h1>${average}</h1>
+  <span>of 100</span>
+  `;
+}
